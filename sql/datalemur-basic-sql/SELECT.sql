df6c2e59c6c07a4714fb54cfb0b23069ef1ee162
Rushikesh Pandurang Dhumal <rd1113@scarletmail.rutgers.edu>
2026-02-21 20:49:38 -0500

    Update sql/datalemur-basic-sql: SELECT

---
+++ sql/datalemur-basic-sql/SELECT.sql
@@ -0,0 +1,10 @@
+/*
+SQL Select Practice Exercise
+https://datalemur.com/questions/sql-select-practice-exercise
+
+Your given a products table, which contains data about different Microsoft Azure cloud products.
+
+Output all the data, in all the columns.
+*/
+
+SELECT * FROM products;
