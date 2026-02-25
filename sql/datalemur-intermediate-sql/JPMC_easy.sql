0ad5f39fec3bfcd6645701d7b7f529a4db030f95
Rushikesh Pandurang Dhumal <rd1113@scarletmail.rutgers.edu>
2026-02-24 19:45:19 -0500

    Update sql/datalemur-intermediate-sql: JPMC Easy

---
+++ sql/datalemur-intermediate-sql/JPMC_easy.sql
@@ -0,0 +1,13 @@
+/*
+Cards Issued Difference
+https://datalemur.com/questions/cards-issued-difference
+
+Your team at JPMorgan Chase is preparing to launch a new credit card, and to gain some insights, you're analyzing how many credit cards were issued each month.
+
+Write a query that outputs the name of each credit card and the difference in the number of issued cards between the month with the highest issuance cards and the lowest issuance. Arrange the results based on the largest disparity.
+*/
+
+SELECT card_name, max(issued_amount) - min(issued_amount) as difference
+FROM monthly_cards_issued
+GROUP BY 1
+ORDER BY difference DESC;
